19b2d89777c05197c91c54a90bb6a11c701a5524
Ilker <ilkerdurmush19b@gmail.com>
2025-06-02 15:11:53 +0300

    Add UserDetails component to display detailed user info

---
+++ src/components/UserDetails.tsx
@@ -0,0 +1,31 @@
+import type { User } from "../types/User";
+
+interface UserDetailsProps {
+  user: User;
+}
+
+const UserDetails = ({ user }: UserDetailsProps) => {
+  return (
+    <div>
+      <h2>Name: {user.name}</h2>
+      <p>Username: @{user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>
+        Website:{" "}
+        <a href={`https://${user.website}`} target="_blank" rel="noreferrer">
+          {user.website}
+        </a>
+      </p>
+      <p>
+        Address: {user.address.suite}, {user.address.street},{" "}
+        {user.address.city}, {user.address.zipcode}
+      </p>
+      <p>
+        Company: {user.company.name} — "{user.company.catchPhrase}"
+      </p>
+    </div>
+  );
+};
+
+export default UserDetails;
